6bc6ea68cad599d82f8943ab8a8c4cba1782828c
maelstrom <maelstrom@maelstrom.dev>
2026-03-25 20:26:13 +0100

    refactor: struct_encode

--- include/splice/detail/flux/struct_encode.hpp
+++ include/splice/detail/flux/struct_encode.hpp
@@ -32,49 +32,78 @@ namespace splice::detail
     return flux::encode_intermediary<SerializationTarget>(object.*MemberPointer);
   }
 
+  template<typename StructType>
+  consteval std::size_t serializable_field_count()
+  {
+    // Check whether to mark fields as serializable by default or not
+    bool serializable_by_default = splice::detail::has_annotation<flux::serializable>(^^StructType);
+
+    // Collect only non-static data members
+    constexpr static auto members = std::define_static_array(
+        std::meta::nonstatic_data_members_of(^^StructType, std::meta::access_context::current()));
+
+    std::size_t count = 0;
+    template for (constexpr auto member: members)
+    {
+      // Require that the member either be annotated with splice::flux::field or its parent struct be
+      // annotated with splice::flux::serializable, and that it is public and has an identifier
+      if ((serializable_by_default || splice::detail::has_annotation<splice::flux::field>(member))
+          && std::meta::is_public(member) && std::meta::has_identifier(member))
+      {
+        count++;
+      }
+    }
+    return count;
+  }
+
+  template<typename SerializationTarget, typename StructType>
+  struct member_encoder
+  {
+    std::string_view member_name;
+    MemberEncoder<typename SerializationTarget::IntermediaryType, StructType> *encode;
+  };
+
+  template<typename SerializationTarget, typename StructType>
+  consteval std::array<member_encoder<SerializationTarget, StructType>, serializable_field_count<StructType>()>
+  get_member_encoders()
+  {
+    // Check whether to mark fields as serializable by default or not
+    bool serializable_by_default = splice::detail::has_annotation<flux::serializable>(^^StructType);
+
+    constexpr static auto members = std::define_static_array(
+        std::meta::nonstatic_data_members_of(^^StructType, std::meta::access_context::current()));
+
+    std::array<member_encoder<SerializationTarget, StructType>, serializable_field_count<StructType>()> member_encoders;
+    std::size_t i = 0;
+    template for (constexpr auto member: members)
+    {
+      // Require that the member either be annotated with splice::flux::field or its parent struct be
+      // annotated with splice::flux::serializable, and that it is public and has an identifier
+      if ((serializable_by_default || splice::detail::has_annotation<splice::flux::field>(member))
+          && std::meta::is_public(member) && std::meta::has_identifier(member))
+      {
+        using MemberType = typename[:std::meta::type_of(member):];
+        member_encoders[i++] = member_encoder<SerializationTarget, StructType> {
+          .member_name = std::meta::identifier_of(member),
+          .encode = &struct_member_encoder<SerializationTarget, StructType, MemberType, &[:member:]>,
+        };
+      }
+    }
+
+    return member_encoders;
+  }
+
   template<typename SerializationTarget, typename StructType>
   SerializationTarget::IntermediaryType encode_struct(StructType object)
   {
-    constexpr std::array member_encoders = []
-    {
-      constexpr static auto all_members = std::define_static_array(
-          std::meta::nonstatic_data_members_of(^^StructType, std::meta::access_context::current()));
-      constexpr static std::size_t count = []()
-      {
-        std::size_t count = 0;
-        template for (constexpr auto member: all_members)
-        {
-          if (splice::detail::has_annotation<splice::flux::field>(member))
-          {
-            count++;
-          }
-        }
-        return count;
-      }();
+    constexpr std::array member_encoders = get_member_encoders<SerializationTarget, StructType>();
 
-      std::array<
-          std::pair<std::string_view, MemberEncoder<typename SerializationTarget::IntermediaryType, StructType> *>,
-          count>
-          member_encoders;
-      std::size_t i = 0;
-      template for (constexpr auto member: all_members)
-      {
-        if (splice::detail::has_annotation<splice::flux::field>(member))
-        {
-          using MemberType = typename[:std::meta::type_of(member):];
-          member_encoders[i++] = std::make_pair(std::meta::identifier_of(member),
-              &struct_member_encoder<SerializationTarget, StructType, MemberType, &[:member:]>);
-        }
-      }
-
-      return member_encoders;
-    }();
-
+    // Apply member encoders to a map
     flux::MapObject<SerializationTarget> result;
-    for (auto encoder_pair: member_encoders)
+    for (auto encoder: member_encoders)
     {
-      std::string key = static_cast<std::string>(std::get<0>(encoder_pair));
-      result[key] = std::get<1>(encoder_pair)(object);
+      std::string key = static_cast<std::string>(encoder.member_name);
+      result[key] = encoder.encode(object);
     }
     return flux::encode_intermediary<SerializationTarget>(result);
   }
